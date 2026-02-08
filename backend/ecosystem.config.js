55adc3d8ad19b3f564fe414385b2acaafa944185
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-02-08 10:02:40 +0600

    chore: Update backend script to use virtual environment Python interpreter.

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "research-lab-backend",
-        script: "python3",
+        script: "./venv/bin/python",
         args: "-m uvicorn app.main:app --host 0.0.0.0 --port 9191",
         cwd: "./"
     }]
